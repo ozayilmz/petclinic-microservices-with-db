9711ed54b2c24714072f32886af45a38f67241fb
ozayilmz <ozanyilmaz.de@gmail.com>
2025-03-17 22:35:03 +0000

    added selenium jobs written in python

---
+++ selenium-jobs/test_owners_all_headless.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+import os
+
+# Headless (ekransız) modda çalışmak için Chrome seçeneklerini ayarla
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("headless")
+chrome_options.add_argument("no-sandbox")
+chrome_options.add_argument("disable-dev-shm-usage")
+
+# Chrome webdriver örneğini başlat ve seçenekleri ekle
+driver = webdriver.Chrome(options=chrome_options)
+
+# Uygulamaya bağlan
+APP_IP = os.environ['MASTER_PUBLIC_IP']
+url = "http://" + APP_IP.strip() + ":8080/"
+print(url)
+driver.get(url)
+sleep(3)
+
+# "OWNERS" linkine tıkla
+owners_link = driver.find_element("link text", "OWNERS")
+owners_link.click()
+sleep(2)
+
+# "ALL" linkine tıkla
+all_link = driver.find_element("link text", "ALL")
+all_link.click()
+sleep(2)
+
+# Tablo yüklendiğini doğrula
+sleep(1)
+verify_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "table")))
+
+print("Table loaded")
+
+# Tarayıcıyı kapat
+driver.quit()
---
+++ selenium-jobs/test_owners_register_headless.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import random
+import os
+
+# Headless (ekransız) modda çalışmak için Chrome seçeneklerini ayarla
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("headless")
+chrome_options.add_argument("no-sandbox")
+chrome_options.add_argument("disable-dev-shm-usage")
+
+# Chrome webdriver örneğini başlat ve seçenekleri ekle
+driver = webdriver.Chrome(options=chrome_options)
+
+# Uygulamaya bağlan
+APP_IP = os.environ['MASTER_PUBLIC_IP']
+url = "http://" + APP_IP.strip() + ":8080/"
+print(url)
+driver.get(url)
+
+# "OWNERS" linkine tıkla
+owners_link = driver.find_element("link text", "OWNERS")
+owners_link.click()
+sleep(2)
+
+# "REGISTER" linkine tıkla
+all_link = driver.find_element("link text", "REGISTER")
+all_link.click()
+sleep(2)
+
+# Yeni bir sahibi Petclinic uygulamasına kaydet
+fn_field = driver.find_element_by_name('firstName')
+fn = 'Callahan' + str(random.randint(0, 100))
+fn_field.send_keys(fn)
+sleep(1)
+
+fn_field = driver.find_element_by_name('lastName')
+fn_field.send_keys('Clarusway')
+sleep(1)
+
+fn_field = driver.find_element_by_name('address')
+fn_field.send_keys('Ridge Corp. Street')
+sleep(1)
+
+fn_field = driver.find_element_by_name('city')
+fn_field.send_keys('McLean')
+sleep(1)
+
+fn_field = driver.find_element_by_name('telephone')
+fn_field.send_keys('+1230576803')
+sleep(1)
+
+fn_field.send_keys(Keys.ENTER)
+
+# Yeni eklenen sahip listesinin güncellenmesi için 10 saniye bekle
+sleep(10)
+
+# Yeni kullanıcının Sahipler Tablosunda olup olmadığını doğrula
+if fn in driver.page_source:
+    print(fn, 'is added and found in the Owners Table')
+    print("Test Passed")
+else:
+    print(fn, 'is not found in the Owners Table')
+    print("Test Failed")
+
+# Tarayıcıyı kapat
+driver.quit()
